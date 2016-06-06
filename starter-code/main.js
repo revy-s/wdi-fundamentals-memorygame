2c603383fe58ee4b46394e8d59b8fa558f887b74
RDSamia-MBP <RDSamia@RDSamia-MBPs-MacBook-Pro.local>
2016-06-06 16:14:01 -0700

    Added if else statement

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,12 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardOne === cardThree && cardOne === cardFour) {
+  window.alert("Sorry try again.");
+}
+else if (cardTwo === cardThree && cardTwo === cardFour) {
+  window.alert("Sorry try again.");
+} else {
+  window.alert("You found a match.");
+}
