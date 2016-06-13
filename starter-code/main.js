dfc5970305b9d14b754263a21cb0507ca2d994e9
RDSamia-MBP <RDSamia@RDSamia-MBPs-MacBook-Pro.local>
2016-06-13 10:02:37 -0700

    Added four div class cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,6 +5,7 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+/*DO NOT DELETE
 if (cardOne === cardThree && cardOne === cardFour) {
   window.alert("Sorry try again.");
 }
@@ -13,3 +14,42 @@ else if (cardTwo === cardThree && cardTwo === cardFour) {
 } else {
   window.alert("You found a match.");
 }
+*/
+
+var gameBoard = document.getElementById('game-board');
+
+document.getElementById('game-board');
+//<div id="game-board" class="board"></div>
+
+/*FOLLOWING IS LONG VERSION TO CREATE FOUR <div class="card"></div>
+CREATE four '<div></div>' element
+var cCards = document.createElement('div');
+	var cCard1 = document.createElement('div');
+	var cCard2 = document.createElement('div');
+	var cCard3 = document.createElement('div');
+	var cCard4 = document.createElement('div');
+
+CREATE four 'class="card"' element
+cCards.className = 'card';
+	cCard1.className = 'card';
+	cCard2.className = 'card';
+	cCard3.className = 'card';
+	cCard4.className = 'card';
+
+ADD four <div class="card"></div> after 'div id=game-board...'
+document.getElementById('game-board').appendChild(cCards);
+	document.getElementById('game-board').appendChild(cCard1);
+	document.getElementById('game-board').appendChild(cCard2);
+	document.getElementById('game-board').appendChild(cCard3);
+	document.getElementById('game-board').appendChild(cCard4);
+*/
+
+/*Following 'FOR LOOP' below was from http://stackoverflow.com/questions/11398522/create-a-div-using-loop
+*/
+var toAddCards = document.createDocumentFragment();
+for (var i = 0; i < 4; i++) {
+	var cCards = document.createElement('div');
+	cCards.className = 'card';
+	toAddCards.appendChild(cCards);
+}
+document.getElementById('game-board').appendChild(toAddCards);
